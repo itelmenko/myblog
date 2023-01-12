3d9314d220313b77f45adff877c49230d3c2b09d
Igor Telmenko <igor-telmenko@yandex.ru>
2023-01-12 08:11:29 +0300

    Изменения vite.config.js

---
+++ examples/vue3-laravel-docker/vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    /*server: {
+        host: '192.168.128.5'
+    },*/
+    plugins: [
+        laravel(['resources/js/app.js', 'resources/css/app.css']),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    // The Vue plugin will re-write asset URLs, when referenced
+                    // in Single File Components, to point to the Laravel web
+                    // server. Setting this to `null` allows the Laravel plugin
+                    // to instead re-write asset URLs to point to the Vite
+                    // server instead.
+                    base: null,
+
+                    // The Vue plugin will parse absolute URLs and treat them
+                    // as absolute paths to files on disk. Setting this to
+                    // `false` will leave absolute URLs un-touched so they can
+                    // reference assets in the public directory as expected.
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+});
