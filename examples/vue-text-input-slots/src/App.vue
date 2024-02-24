0c2adebbe1578999d3d17a834317c2866cf734a1
Igor Telmenko <igor-telmenko@yandex.ru>
2024-02-24 20:58:52 +0300

    Блог. Пример с несколькими слотами для текстового поля. Использование слотов

--- examples/vue-text-input-slots/src/App.vue
+++ examples/vue-text-input-slots/src/App.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts">
 import TextInput from "@/components/TextInput.vue";
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faSearch, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 </script>
 
 <template>
-  <h1>Text file with icons and buttons</h1>
-  <p>Lorem ipsum</p>
-  <TextInput></TextInput>
+  <h1>Text fields with icons and buttons</h1>
+  <div class="row">Lorem ipsum ...</div>
+  <div class="row">
+    <TextInput></TextInput>
+  </div>
+  <div class="row">
+    <TextInput>
+      <template #prepend><FontAwesomeIcon :icon="faSearch" class="icon" /></template>
+      <template #append><input type="submit" value="Search" class="button" /></template>
+    </TextInput>
+  </div>
+  <div class="row">
+    <TextInput>
+      <template #append><FontAwesomeIcon :icon="faEnvelope" class="icon" /></template>
+    </TextInput>
+  </div>
 </template>
+
+<style scoped>
+.row + .row {
+  margin-top: 12px;
+}
+</style>
--- examples/vue-text-input-slots/src/components/TextInput.vue
+++ examples/vue-text-input-slots/src/components/TextInput.vue
@@ -1,12 +1,9 @@
-<script setup lang="ts">
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="text-input-container">
-    <FontAwesomeIcon :icon="faSearch" class="icon" />
-    <input type="search" name="search" class="field" placeholder="Search..." />
-    <input type="submit" value="Search" class="button" />
+    <slot name="prepend"></slot>
+    <input type="search" name="search" class="field"/>
+    <slot name="append"></slot>
   </div>
 </template>
