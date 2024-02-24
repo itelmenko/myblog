25c79aa1c7686f196299c8ef16d10678b70579d9
Igor Telmenko <igor-telmenko@yandex.ru>
2024-02-24 17:15:43 +0300

    Блог. Пример с несколькими слотами для текстового поля. Переработаны стили

--- examples/vue-text-input-slots/src/components/TextInput.vue
+++ examples/vue-text-input-slots/src/components/TextInput.vue
@@ -5,8 +5,8 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 <template>
   <div class="text-input-container">
-    <div class="text-input-prepend-icon"><FontAwesomeIcon :icon="faSearch" /></div>
-    <input class="text-input-field" type="search" name="search" placeholder="Search..." />
-    <input type="submit" value="Search" class="text-input-append-button" />
+    <FontAwesomeIcon :icon="faSearch" class="icon" />
+    <input type="search" name="search" class="field" placeholder="Search..." />
+    <input type="submit" value="Search" class="button" />
   </div>
 </template>
