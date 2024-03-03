1e9ee35256b24222e4f90cfbc53b9d87395b64e3
Igor Telmenko <igor-telmenko@yandex.ru>
2024-03-03 18:49:55 +0300

    Таблица данных на Vue. Комментарии

--- examples/vue-data-table/src/api/helpers.js
+++ examples/vue-data-table/src/api/helpers.js
@@ -1,3 +1,4 @@
+// Преобразование данных компонента в параметры API
 export function covertDataTableOptionsToQueryParams(opts) {
   const result = {
     _limit: opts.itemsPerPage,
@@ -12,6 +13,7 @@ export function covertDataTableOptionsToQueryParams(opts) {
   return result
 }
 
+// Преобразование результата API в данные для компонента
 export function convertApiResponceToDataTableOptions(data) {
   return { items: data.data, total: data.headers['x-total-count'] }
 }
