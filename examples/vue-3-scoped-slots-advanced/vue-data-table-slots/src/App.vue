91fe0149cd38360a910372d2d96bda1433deab0d
Igor Telmenko <igor-telmenko@yandex.ru>
2024-03-17 16:07:10 +0300

    Таблица данных на Vue. Перед добавлением слотов

---
+++ examples/vue-3-scoped-slots-advanced/vue-data-table-slots/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Vue3 DataTable</h1>
+  <PostDataTable></PostDataTable>
+</template>
+<script setup>
+import PostDataTable from '@/components/PostDataTable.vue'
+</script>
---
+++ examples/vue-3-scoped-slots-advanced/vue-data-table-slots/src/components/PostDataTable.vue
@@ -0,0 +1,28 @@
+<template>
+  <data-table :headers="headers" :load-data-function="getPosts"></data-table>
+</template>
+
+<script setup>
+import { getPosts } from '@/api/posts.js'
+import DataTable from '@/components/DataTable.vue'
+
+/**
+ * Настройки колонок
+ */
+const headers = [
+  {
+    title: 'ID',
+    align: 'end',
+    sortable: true,
+    key: 'id',
+    width: '5em'
+  },
+  {
+    title: 'Title',
+    align: 'start',
+    sortable: true,
+    key: 'title',
+    width: '100%'
+  }
+]
+</script>
