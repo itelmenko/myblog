cbe0e2662374fb63d4195d35c4b1bf92c06e5475
Igor Telmenko <igor-telmenko@yandex.ru>
2024-03-27 22:13:51 +0300

    Vue 3 expose. Примеры

---
+++ examples/p8417/vue-expose-counter/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <main>
+    <my-counter ref="counter"></my-counter>
+    <button @click="counter.reset()">Reset</button>
+  </main>
+  <footer>
+    <button @click="counter.increment()">counter.increment()</button>
+  </footer>
+</template>
+<script>
+import MyCounter from '@/components/MyCounter.vue'
+import { ref } from 'vue'
+
+export default {
+  components: {
+    MyCounter
+  },
+  setup() {
+    const counter = ref()
+
+    return { counter }
+  }
+}
+</script>
+
+<style>
+#app {
+  margin: 0 auto;
+  padding: 15px;
+  width: 360px;
+  border: 1px gainsboro solid;
+  border-radius: 6px;
+}
+footer {
+  margin-top: 15px;
+}
+label + button,
+button + button {
+  margin-inline-start: 5px;
+}
+</style>
---
+++ examples/p8417/vue-expose-counter/src/components/MyCounter.vue
@@ -0,0 +1,24 @@
+<template>
+  <label>Counter: <input type="text" readonly :value="count" /></label>
+  <button @click="increment()">+</button>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup(props, { expose }) {
+    const count = ref(0)
+    const reset = () => (count.value = 0)
+    const increment = () => count.value++
+
+    // Только метод reset будет доступен извне, например через $refs
+    expose({
+      reset
+    })
+
+    // Внутри у шаблона будет доступ к count и increment
+    return { count, increment }
+  }
+}
+</script>
