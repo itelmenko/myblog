f6ffcd12fa4b66c8e2acab2e729829ec73fdbf86
Igor Telmenko <igor-telmenko@yandex.ru>
2024-04-04 20:20:46 +0300

    Vue. Расширенная работа со слотами. Убраны лишние элементы и стили

--- examples/p8449/slot-examples/src/views/HomeView.vue
+++ examples/p8449/slot-examples/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <h1>This is a home page</h1>
+  </div>
 </template>
